a4b5992295cd3c63aa9b37af9b0de67ed40ebe85
vdorbs <victor.d.dorobantu@gmail.com>
2016-02-24 23:26:49 -0500

    Added single player unity example

---
+++ SinglePlayer/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	private Rigidbody rb;
+
+	void Start() {
+		rb = GetComponent<Rigidbody>();
+	}
+
+	void Update() {
+		float x = Input.GetAxis ("Horizontal");
+		float z = Input.GetAxis ("Vertical");
+
+		rb.AddForce (new Vector3 (x, 0.0f, z) * 10);
+	}
+}
