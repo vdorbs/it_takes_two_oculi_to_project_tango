a6a4d867de6d0af7a3c00330d7fcbc880d3eee93
Jared Exter <jpe47@cornell.edu>
2016-03-01 23:37:33 -0500

    added tango project

---
+++ Tango/Tango/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading;
+
+namespace Tango
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello World!");
+			Thread.Sleep (10000);
+		}
+	}
+}
