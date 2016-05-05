df9ad8f7443e0415c79ef72e384bf68bb3013139
Jared Exter <jpe47@cornell.edu>
2016-05-04 23:00:13 -0400

    added program

--- Tango/Tango/Program.cs
+++ Tango/Tango/Program.cs
@@ -34,58 +34,31 @@ namespace Tango
 
 	class MainClass
 	{
+		//GROUP PARAMS
 		public const int UPDATE = 0;
 		public const int LOOKUP = 1;
 		public const int REFRESH = 2;
 		public const int LEAVE_ROOM = 3;
 		public const int JOIN_ROOM = 4;
+
+		//MAX IN ROOM ALLOWANCE
 		public const int MAX_IN_ROOM = 5;
+
 		public static Room currentRoom;
 		private static NetworkStream stream;
+		public static TcpClient client;
+		public static Vsync.Group roomGroup;
+		public static String ID;
 
+		//Whether the client has made contact recently
+		public static Boolean still_alive = false;
+		public const int CLIENT_TIMEOUT = 5000;
+		public const int UPDATE_RATE = 50;
 
-		public static Vsync.Group createRoomGroup (string name) {
-			Vsync.Group roomGroup = new Vsync.Group (name);
-			roomGroup.ViewHandlers += (ViewHandler)delegate(View v) {
-				VsyncSystem.WriteLine("New View: " + v);
-				Console.Title = "View " + v.viewid + ", my rank=" + v.GetMyRank();
-			};
-			roomGroup.Handlers[UPDATE] += (Action<string,string>) delegate(string username, string val) {
-				//VsyncSystem.WriteLine("IN UPDATE");
-				lock (currentRoom) {
-					currentRoom.playerLocs[username] = val;
-				}
-			};
-			roomGroup.Handlers[LOOKUP] += (Action<string>)delegate(string s) {
-				//VsyncSystem.WriteLine("IN LOOKUP");
-				lock (currentRoom) {
-					roomGroup.Reply(currentRoom.playerLocs[s]);
-				}
-			};
-			roomGroup.Handlers [REFRESH] += (Action)delegate() {
-				lock (currentRoom) {
-					string reply = Extensions.FromDictionaryToJson(currentRoom.playerLocs);
-					roomGroup.Reply(reply);
-				}
-			};
-			roomGroup.Handlers [JOIN_ROOM] += (Action<string>)delegate(string username) {
-				lock (currentRoom) {
-					if (currentRoom.players.Count >= MAX_IN_ROOM) {
-						roomGroup.Reply("FULL");
-					}
-					else {
-						currentRoom.players.Add(username);
-						roomGroup.Reply("JOINED");
-					}
-				}
-			};
-			roomGroup.Handlers [LEAVE_ROOM] += (Action<string>)delegate(string s) {
-				lock (currentRoom) {
-					currentRoom.playerLocs.Remove(s);
-				}
-			};
-			return roomGroup;
-		}
+		//System Timers
+		public static System.Timers.Timer send_timer;
+		public static System.Timers.Timer is_alive_timer;
+
 
 		//checks to see if the message wants you to change rooms
 		public static bool isChangeRoomMsg(String msg) {
@@ -107,7 +80,13 @@ namespace Tango
 				}
 				Byte[] r = Encoding.UTF8.GetBytes(info);
 				Byte[] resp = Helpers.Connections.encode(r);
-				stream.Write(resp, 0, resp.Length);
+				try {
+					stream.Write(resp, 0, resp.Length);
+				}
+				catch (Exception exception) {
+					Console.WriteLine("Caught an exception in send_info, pretty much just ignoring. It was:");
+					Console.WriteLine(exception.ToString());
+				}
 			}
 			catch(Exception err){
 				//Fail Loudly
@@ -115,19 +94,33 @@ namespace Tango
 			}
 		}
 
+		/*public static void is_alive(Object source, System.Timers.ElapsedEventArgs e) {
+			Console.WriteLine ("IN IS_ALIVE METHOD");
+			if (still_alive) {
+				still_alive = false;
+			} else {
+				Console.WriteLine ("CLOSING CONNECTION BECAUSE NO RESPONSE");
+				roomGroup.OrderedSend (LEAVE_ROOM, ID);
+				send_timer.Stop ();
+				is_alive_timer.Stop ();
+				client.Close ();
+				roomGroup.Leave ();
+			}
+			
+		}*/
+
 		public static void Main (string[] args)
 		{
 			String roomname = "no roomname given";
 			Console.WriteLine ("THE SIZE OF ARGS IS " + args.Length);
 			if (args.Length < 2) {
 				Console.WriteLine ("There are less than 2 args given.  This is fine for now, but in the future we need to give the room name.");
-			} 
-			else {
-				roomname = args[1];
+			} else {
+				roomname = args [1];
 				int max_port = 65000;
 				int min_port = 15000;
 				int port_diff = max_port - min_port;
-				int room_hash = roomname.GetHashCode();
+				int room_hash = roomname.GetHashCode ();
 				room_hash = room_hash % port_diff;
 				room_hash = room_hash + port_diff;
 				room_hash = room_hash % port_diff;
@@ -138,30 +131,68 @@ namespace Tango
 			}
 				
 			currentRoom = new Room (roomname);
-			String ID = Path.GetRandomFileName ().Replace (".", "");
+			ID = Path.GetRandomFileName ().Replace (".", "");
 			Console.WriteLine ("User ID IS " + ID);
 
 			//Set up TCP Listener
-			Console.WriteLine(args[0]);
+			Console.WriteLine (args [0]);
 			int port = Int32.Parse (args [0]);
 			TcpListener server = new TcpListener (port);
 			server.Start ();
-			Console.WriteLine("Server has started on 127.0.0.1:{0}.\nWaiting for a connection...", port);
-			TcpClient client = server.AcceptTcpClient ();
-			stream = client.GetStream();
+			Console.WriteLine ("Server has started on 127.0.0.1:{0}.\nWaiting for a connection...", port);
+			client = server.AcceptTcpClient ();
+			stream = client.GetStream ();
 			Console.WriteLine ("Matched with a client! Now Starting VSYNC");
 
 			VsyncSystem.Start ();
 			Console.WriteLine ("VSYNC STARTED");
 			String groupName = "TEST ROOM";
 
-			Vsync.Group roomGroup = createRoomGroup (groupName);
+			roomGroup = new Vsync.Group (groupName);
+			roomGroup.ViewHandlers += (ViewHandler)delegate(View v) {
+				VsyncSystem.WriteLine ("New View: " + v);
+				Console.Title = "View " + v.viewid + ", my rank=" + v.GetMyRank ();
+			};
+			roomGroup.Handlers [UPDATE] += (Action<string,string>)delegate(string username, string val) {
+				lock (currentRoom) {
+					currentRoom.playerLocs [username] = val;
+				}
+			};
+			roomGroup.Handlers [LOOKUP] += (Action<string>)delegate(string s) {
+				//VsyncSystem.WriteLine("IN LOOKUP");
+				lock (currentRoom) {
+					roomGroup.Reply (currentRoom.playerLocs [s]);
+				}
+			};
+			roomGroup.Handlers [REFRESH] += (Action)delegate() {
+				lock (currentRoom) {
+					string reply = Extensions.FromDictionaryToJson (currentRoom.playerLocs);
+					roomGroup.Reply (reply);
+				}
+			};
+			roomGroup.Handlers [JOIN_ROOM] += (Action<string>)delegate(string username) {
+				lock (currentRoom) {
+					if (currentRoom.players.Count >= MAX_IN_ROOM) {
+						roomGroup.Reply ("FULL");
+					} else {
+						currentRoom.players.Add (username);
+						roomGroup.Reply ("JOINED");
+					}
+				}
+			};
+			roomGroup.Handlers [LEAVE_ROOM] += (Action<string>)delegate(string s) {
+				Console.WriteLine("IN LEAVE ROOM");
+				lock (currentRoom) {
+					currentRoom.playerLocs.Remove (s);
+					currentRoom.players.Remove(s);
+				}
+			};
 			roomGroup.Join ();
 			Console.WriteLine ("Room Group Joined");
 			Console.WriteLine (groupName);
 
 			List<String> results = new List<String> ();
-			roomGroup.OrderedQuery(Vsync.Group.ALL, JOIN_ROOM, ID, new Vsync.EOLMarker(), results); 
+			roomGroup.OrderedQuery (Vsync.Group.ALL, JOIN_ROOM, ID, new Vsync.EOLMarker (), results); 
 			Boolean full = false;
 			for (int j = 0; j < results.Count; j++) {
 				if (results [j].Equals ("FULL")) {
@@ -171,70 +202,89 @@ namespace Tango
 			}
 			if (full) {
 				roomGroup.OrderedSend (LEAVE_ROOM, ID);
-				Byte[] r = Encoding.UTF8.GetBytes("ROOM FULL");
-				Byte[] resp = Helpers.Connections.encode(r);
-				stream.Write(resp, 0, resp.Length);
+				Byte[] r = Encoding.UTF8.GetBytes ("ROOM FULL");
+				Byte[] resp = Helpers.Connections.encode (r);
+				stream.Write (resp, 0, resp.Length);
+				client.Close ();
+				Console.WriteLine ("LEAVING ROOM");
+				roomGroup.Leave ();
 				return;
 			}
 								
-			System.Timers.Timer t = new System.Timers.Timer ();
-			t.Elapsed += send_info;
-			t.Interval = 50;
-			t.Start ();
+			send_timer = new System.Timers.Timer ();
+			send_timer.Elapsed += send_info;
+			send_timer.Interval = UPDATE_RATE;
+			send_timer.Start ();
+
+			/*is_alive_timer = new System.Timers.Timer ();
+			is_alive_timer.Elapsed += is_alive;
+			is_alive_timer.Interval = CLIENT_TIMEOUT;
+			is_alive_timer.Start ();*/
 
 			Byte[] bytes = new Byte[4096];
 			String data = null;
 
-			int i = Helpers.Connections.checkRead(stream, bytes);
+			int i = Helpers.Connections.checkRead (stream, bytes);
 			//Continuously read from the buffer while the connection is open 
 			while (i != 0) {
-				data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+				data = System.Text.Encoding.ASCII.GetString (bytes, 0, i);
 				//First request -> Respond with HTTP Change Protocol Handshake 
 				if (new Regex ("^GET").IsMatch (data)) {
 					Byte[] response = Encoding.UTF8.GetBytes ("HTTP/1.1 101 Switching Protocols" + Environment.NewLine
-						+ "Connection: Upgrade" + Environment.NewLine
-						+ "Upgrade: websocket" + Environment.NewLine
-						+ "Sec-WebSocket-Accept: " + Convert.ToBase64String (
-							SHA1.Create ().ComputeHash (
-								Encoding.UTF8.GetBytes (
-									new Regex ("Sec-WebSocket-Key: (.*)").Match (data).Groups [1].Value.Trim () + "258EAFA5-E914-47DA-95CA-C5AB0DC85B11"
-								)
-							)
-						) + Environment.NewLine
-						+ Environment.NewLine);
+					                  + "Connection: Upgrade" + Environment.NewLine
+					                  + "Upgrade: websocket" + Environment.NewLine
+					                  + "Sec-WebSocket-Accept: " + Convert.ToBase64String (
+						                  SHA1.Create ().ComputeHash (
+							                  Encoding.UTF8.GetBytes (
+								                  new Regex ("Sec-WebSocket-Key: (.*)").Match (data).Groups [1].Value.Trim () + "258EAFA5-E914-47DA-95CA-C5AB0DC85B11"
+							                  )
+						                  )
+					                  ) + Environment.NewLine
+					                  + Environment.NewLine);
 
 					stream.Write (response, 0, response.Length);
 
 					//Generate and send user ID
-					Byte[] q = Encoding.UTF8.GetBytes(ID);
+					Byte[] q = Encoding.UTF8.GetBytes (ID);
 					//Byte[] resp = Helpers.Connections.encode(q);
-					Byte[] resp = Helpers.Connections.encode(q);
-					stream.Write(resp, 0, resp.Length);
+					Byte[] resp = Helpers.Connections.encode (q);
+					stream.Write (resp, 0, resp.Length);
 
 					//Start the timer and send dictionary
-					i = Helpers.Connections.checkRead(stream, bytes);
+					i = Helpers.Connections.checkRead (stream, bytes);
 
-				}  else {
+				} else {
 					//decode input and put into dictionary if valid location data
 					Byte[] decoded = Helpers.Connections.decode (bytes);
 					//string s = System.Text.Encoding.UTF8.GetString(decoded, 0, decoded.Length);
 					//Console.WriteLine ("RECEIVED MESSAGE " + s);
-					String response = Helpers.Connections.parseAndPut(decoded);
+					String response = Helpers.Connections.parseAndPut (decoded);
 					//Console.WriteLine ("Received these coordinates" + response);
 					if (response == null) {
 						Console.WriteLine ("NULL STUFF RECEIVED");
 					} else {
+						still_alive = true;
 						roomGroup.OrderedSend (UPDATE, ID, response); 
 					}
-					i = Helpers.Connections.checkRead(stream, bytes);
+					i = Helpers.Connections.checkRead (stream, bytes);
 					//Console.WriteLine ("Decoded: {0}", System.Text.Encoding.UTF8.GetString (decoded, 0, decoded.Length));
 				}
 			}
 
 			Console.WriteLine ("Reached end of input");
-			t.Stop ();
+			Console.WriteLine ("NEW");
+			try {
+				roomGroup.OrderedSend (LEAVE_ROOM, ID);
+			}
+			catch (Exception) {
+				Console.WriteLine ("WE HAVE AN ERROR BUT WE ARE IGNORING IT NOW");
+			}
+			send_timer.Stop ();
+			//is_alive_timer.Stop ();
+			Console.WriteLine ("ROOM GROUP LEAVE"); 
+			//roomGroup.Leave();
 			client.Close ();
-		
+			//Console.WriteLine ("HERE 3");
 		}
 	}
 }
